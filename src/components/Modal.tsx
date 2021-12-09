4cd0a31522b8c977f8563a6e82bef4b262a8b6a6
saurabh.s3 <saurabh.s@fampay.in>
2021-12-09 12:05:59 +0530

    added show more toggle

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { buttonStyles } from "../styles/classNames";
 import Form from "./Form";
 import Store from "../store/Store";
 import { observer } from "mobx-react-lite";
 
 const Modal = () => {
+  const [showMoreToggle, setShowMoreToggle] = useState<boolean>(false);
   const { RuleGroups, addRuleGroup, getQueryString, getQueryObject } =
     useContext(Store);
   const QueryString = getQueryString();
@@ -15,15 +16,35 @@ const Modal = () => {
     alert("Please check console");
   };
 
+  const handleShowMoreToggle = () => {
+    setShowMoreToggle(!showMoreToggle);
+  };
+
   return (
     <div className="h-4/5 bg-black rounded-sm w-8/12 shadow-lg flex flex-col">
       <div className="bg-primary px-8 py-6">
         <p className="font-medium text-white">Create tag and query</p>
         {RuleGroups[0].group.children[0].field ? (
-          <p className="text-white text-sm my-1 mx-0.5 bg-indigo-700 p-2 rounded font-medium">
-            <span className="font-bold">Query: </span>
-            {QueryString}
-          </p>
+          <div
+            className={`${
+              showMoreToggle ? "flex-col" : "flex-row items-center"
+            } flex w-full`}
+          >
+            <p
+              className={` ${
+                showMoreToggle ? "" : "truncate"
+              } flex-grow text-white text-sm my-1 mx-0.5 bg-indigo-700 p-2 rounded font-medium`}
+            >
+              <span className="font-bold">Query: </span>
+              {QueryString}
+            </p>
+            <span
+              className="text-white text-sm font-medium ml-2 flex-grow-0 cursor-pointer"
+              onClick={handleShowMoreToggle}
+            >
+              {showMoreToggle ? "less" : "more"}...
+            </span>
+          </div>
         ) : (
           <p className="text-indigo-300 text-sm m-0.5">
             The query you build will be saved in your active view
